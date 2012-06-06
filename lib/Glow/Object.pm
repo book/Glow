7e4d6517088f7a08affeb8e5657a079202b65456
Philippe Bruhat (BooK) <book@cpan.org>
2012-06-06 23:49:07 +0200

    add support for a 'source' constructor parameter
    this will be turned into a coderef suitable for content_source

--- lib/Glow/Object.pm
+++ lib/Glow/Object.pm
@@ -13,10 +13,31 @@ has kind => ( is => 'ro', isa => 'Str', lazy_build => 1, init_arg => undef );
 # these attributes can be generated, and need not to be set in the constructor
 has size => ( is => 'ro', isa => 'Int', lazy_build => 1, required => 0 );
 has sha1 => ( is => 'ro', isa => 'Str', lazy_build => 1, required => 0 );
+
+# these attributes define the content, from which almost everything derives:
+# - raw content, as a string
 has content => ( is => 'ro', isa => 'Str', lazy_build => 1, required => 0 );
 
-# a coderef that will always return a filehandle pointing at the beginning of the content
+# - a coderef that returns a filehandle pointing at the content beginning
+#   (BUILDARGS supports a 'source' parameter to pass in a filename instead)
 has content_source => ( is => 'ro', isa => 'CodeRef', required => 0, predicate => 'has_content_source' );
+
+# turn the 'source' parameter into a 'content_source' parameter
+around BUILDARGS => sub {
+    my $orig  = shift;
+    my $class = shift;
+    my $args  = $class->$orig(@_);
+    if( exists $args->{source} ) {
+        my $source = delete $args->{source};
+        die "$source does not exist" if !-e $source;
+        die "$source is unreadable"  if !-r $source;
+        $args->{content_source} = sub {
+            open my $fh, '<', $source or die "Can't open $source: $!";
+            return $fh;
+        };
+    }
+    return $args;
+};
 
 # builders
 sub _build_kind {
