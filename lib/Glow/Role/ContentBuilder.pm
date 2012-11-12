ef718a62fede8a8feb54c4b4a1570370c02c7651
Philippe Bruhat (BooK) <book@cpan.org>
2012-11-12 09:54:51 +0100

    let the method name be built elsewhere

--- lib/Glow/Role/ContentBuilder.pm
+++ lib/Glow/Role/ContentBuilder.pm
@@ -3,15 +3,14 @@ use Moose::Role;
 
 sub _trigger {
     my ( $self, $attribute ) = @_;
-    my $method = "_build_fh_using_$attribute";
     my $error
         = $self->has_content
         ? "Can't provide content with $attribute (content already provided)"
         : $self->content_builder
-        ? "Can't set content_builder to $method (already set to ${\$self->content_builder})"
+        ? "Can't set content_builder to $attribute (already set to ${\$self->content_builder})"
         : '';
     die $error if $error;
-    $self->_set_content_builder($method);
+    $self->_set_content_builder($attribute);
 }
 
 1;
