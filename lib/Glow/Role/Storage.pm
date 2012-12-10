276999aeaeef04f514feb66600a3ec80d6f9e437
Philippe Bruhat (BooK) <book@cpan.org>
2012-12-10 21:20:43 +0100

    _build_readonly is part of the requirements for Glow::Role::Storage

--- lib/Glow/Role/Storage.pm
+++ lib/Glow/Role/Storage.pm
@@ -2,7 +2,7 @@ package Glow::Role::Storage;
 
 use Moose::Role;
 
-requires qw( has_object get_object put_object delete_object );
+requires qw( _build_readonly has_object get_object put_object delete_object );
 
 has readonly => (
     is       => 'ro',
