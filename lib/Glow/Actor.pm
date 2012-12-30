a8c5d2575e92eec4b525d0b4b6a3283e5e3ae3a0
Philippe Bruhat (BooK) <book@cpan.org>
2012-12-30 16:51:42 +0100

    documentation for Glow::Actor

--- lib/Glow/Actor.pm
+++ lib/Glow/Actor.pm
@@ -25,3 +25,37 @@ has ident => (
 sub _build_ident { $_[0]->name . ' <' . $_[0]->email . '>' }
 
 __PACKAGE__->meta->make_immutable;
+
+# ABSTRACT: An actor in Glow
+
+=head1 SYNOPSIS
+
+    use Glow::Actor;
+
+    my $actor = Glow::Actor->new(
+        name  => 'Philippe Bruhat (BooK)',
+        email => 'book@cpan.org'
+    );
+
+    print $actor->ident;    # Philippe Bruhat (BooK) <book@cpan.org>
+
+=head1 DESCRIPTION
+
+L<Glow::Actor> represents a user in L<Glow>, i.e. the combination of a
+name and an email.
+
+=attr name
+
+The name of the L<Glow::Actor>.
+
+=attr email
+
+The email of the L<Glow::Actor>.
+
+=attr ident
+
+The identity of the L<Glow::Actor>, build as:
+
+    Name <email>
+
+=cut
