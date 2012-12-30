e430c654f2a3fdf8a0210937bc012169c6133c5a
Philippe Bruhat (BooK) <book@cpan.org>
2012-12-30 16:52:03 +0100

    documentation for Glow::Config

--- lib/Glow/Config.pm
+++ lib/Glow/Config.pm
@@ -19,3 +19,28 @@ override dir_file => sub {
 };
 
 __PACKAGE__->meta->make_immutable;
+
+# ABSTRACT: Config::GitLike subclass for use with Glow
+
+=pod
+
+=head1 SYNOPSIS
+
+    # $r does Glow::Role::Repository
+    my $config = $r->config;
+
+    # Glow::Config extends Config::GitLike
+    $config->get( key => 'glow.class' );
+
+=head1 DESCRIPTION
+
+L<Glow::Config> is a class to manage L<Glow> configuration. It extends
+L<Config::GitLike> to use a specific name for the configuration file,
+and point back to its L<Glow::Repository> object.
+
+=attr repository
+
+A (weak) link back to the L<Glow::Repository> object the configuration
+belongs to.
+
+=cut
