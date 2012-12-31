d313dee59cee7e5e82a1fb4bb969c102768f4b4f
Philippe Bruhat (BooK) <book@cpan.org>
2012-12-31 11:07:21 +0100

    open() will fail in the basic cases anyway
    no need to program defensively:
    open() will fail if the file does not exist
    or if it is not readable

--- lib/Glow/Role/ContentBuilder/FromFile.pm
+++ lib/Glow/Role/ContentBuilder/FromFile.pm
@@ -13,8 +13,6 @@ has content_from_file => (
 sub _build_fh_using_content_from_file {
     my ($self) = @_;
     my $source = $self->content_from_file;
-    die "$source does not exist" if !-e $source;
-    die "$source is unreadable"  if !-r $source;
     open my $fh, '<', $source or die "Can't open $source: $!";
     return $fh;
 }
