6d7912c429199d4f7428d6e12e53e915f81592a3
Philippe Bruhat (BooK) <book@cpan.org>
2013-01-03 00:03:00 +0100

    documentation for Glow::Repository

--- lib/Glow/Repository.pm
+++ lib/Glow/Repository.pm
@@ -19,3 +19,33 @@ around new => sub {
 sub _build_object_store;    # stub
 
 __PACKAGE__->meta->make_immutable( inline_constructor => 0 );
+
+# ABSTRACT: Factory class to build objects doing Glow::Role::Repository
+
+=pod
+
+=head1 SYNOPSIS
+
+    use Glow::Repository;
+
+    # .git is a Git repository's GIT_DIR
+    my $r = Glow::Repository->new( directory => '.git' );
+
+    # $r is now a Glow::Repository::Git object
+
+=head1 DESCRIPTION
+
+L<Glow::Repository> is a factory class that will instanciate the proper
+repository class based on the repository configuration.
+
+By default, a L<Glow> repository contain a single file named F<config>
+that holds at least this configuration (readable by L<Glow::Config>):
+
+    [glow]
+    	class = Class::Doing::Glow::Role::Repository
+
+By default (see the L</SYNOPSIS>), if the C<[glow]> configuration section
+does not exist in the F<config> file, it is assumed the repository is a
+Git repository, and the class defaults to L<Glow::Repository::Git>.
+
+=cut
