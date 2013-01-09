cc701c5b99e53ef40266dea9089c3caed0f3f0d6
Philippe Bruhat (BooK) <book@cpan.org>
2013-01-09 23:55:54 +0100

    required methods for Glow::Role::Digest

--- lib/Glow/Role/Digest.pm
+++ lib/Glow/Role/Digest.pm
@@ -55,7 +55,7 @@ role {
 
     sub kind {'blob'}
 
-    1;
+    __PACKAGE__->meta->make_immutable;
 
     # or program it
     Moose::Meta::Class->create(
@@ -69,9 +69,22 @@ role {
         methods => { kind => sub {'blob'}, },
     );
 
+    Glow::Repository::Git::Object::Blob->meta->make_immutable;
+
 =head1 DESCRIPTION
 
-L<Glow::Role::Digest> adds a I<digest> attribute to a L<Glow::Object>.
+L<Glow::Role::Digest> adds a I<digest> attribute to a L<Glow> object.
+
+=attr digest
+
+The digest of the object content.
+
+If actually computes the digest of the content, prepended by
+the object I<kind>, a single space character, the ASCII decimal
+representation of the content size, and a NULL byte.
+
+For example, if the object is a C<blob> and the content is C<hello>,
+the digest value is computed from the string C<blob 5\0hello>.
 
 =head1 ROLE PARAMETERS
 
@@ -83,15 +96,26 @@ The I<algorithm> parameter allows you to select the actual digest
 algorithm. Any value that the L<Digest> module recognize is valid
 (as L<Digest> is used in the backend).
 
-=attr digest
+=head1 REQUIRED METHODS
 
-The digest of the object's content.
+This role is usually consumed along with L<Glow::Role::Object>,
+which also provides most of the required methods described below
+(C<kind> being the notable exception).
 
-If actually computes the digest of the content, prepended by
-the object I<kind>, a single space character, the ASCII decimal
-representation of the content size, and a NULL byte.
+=head2 kind
 
-For example, if the object is a C<blob> and the content is C<hello>,
-the digest value is computed on the string C<blob 5\0hello>.
+The object kind.
+
+=head2 size
+
+The object size in bytes.
+
+=head2 content
+
+The object content.
+
+=head2 content_fh
+
+A filehandle from which the content can be read.
 
 =cut
