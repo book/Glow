73bba66ddd651c91fde4547487e2e3960ecf3c2b
Philippe Bruhat (BooK) <book@cpan.org>
2013-01-23 00:11:14 +0100

    Glow::Role::HashFromContent is a role that reads the generic header / body format of content
    With support for the following types of headers:
    - single line, single value
    - multiple lines, multiple values (parent)
    - multiple lines, single value (gpgsig, mergetag)

---
+++ lib/Glow/Role/HashFromContent.pm
@@ -0,0 +1,53 @@
+package Glow::Role::HashFromContent;
+
+use Moose::Role;
+
+requires qw( _header_spec _build_fh_using_attributes );
+
+use Encode qw( decode );
+
+has hash => (
+    is       => 'ro',
+    isa      => 'HashRef',
+    required => 0,
+    lazy     => 1,
+    builder  => '_build_hash',
+    init_arg => undef,
+);
+
+sub _build_hash {
+    my ($self) = @_;
+    my %spec   = $self->_header_spec;
+    my $hash   = {};
+
+    # build the key-value pairs
+    my %header;
+    my @lines = split "\n", $self->content;
+    while ( my $line = shift @lines ) {
+        my ( $key, $value ) = split / /, $line, 2;
+        push @{ $header{$key} }, $value;
+        $header{''} = $header{$key} if $key;    # deal with continuation lines
+    }
+    delete $header{''};
+
+    # deal with encoding (if any)
+    my $encoding = $header{encoding} || 'utf-8';
+
+    # process each pair
+    for my $key ( keys %header ) {
+        my ( $attr, $type ) = @{ $spec{$key} || [ $key, '-' ] };
+        $header{$key} = [ map decode( $encoding, $_ ), @{ $header{$key} } ];
+        $hash->{$attr}
+            = $type eq '-' ? $header{$key}[-1]
+            : $type eq '@' ? $header{$key}
+            : $type eq '=' ? join( "\n", @{ $header{$key} } ) . "\n"
+            :   die "Unknown type $type in $attr handler for $key";
+    }
+
+    # whatever's left is the content body
+    $hash->{body} = join "\n", @lines;
+
+    return $hash;
+}
+
+1;
